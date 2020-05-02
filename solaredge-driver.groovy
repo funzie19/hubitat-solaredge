cd1d7c20c342b61fc641e1b76385efcacb508a8c
Joseph Kregloh <5833499+funzie19@users.noreply.github.com>
2020-05-01 22:37:36 -0400

    Update solaredge-driver.groovy
    Update with improved settings and extra endpoint.

--- solaredge-driver.groovy
+++ solaredge-driver.groovy
@@ -13,7 +13,7 @@
  *
  ***********************************************************************************************************************/
 
-private version() { return "1.0.0" }
+private version() { return "1.1.0" }
 
 metadata {
     definition(
@@ -26,6 +26,10 @@ metadata {
       capability "Power Meter"
       capability "Refresh"
       command "updateTiles"
+      command "getEnergy"
+      command "getOverview"
+      command "getSitePowerFlow"
+      command "clearData"
 
       attribute "production", "number"
     	attribute "consumption", "number"
@@ -36,8 +40,12 @@ metadata {
       attribute "last_month", "number"
       attribute "last_year", "number"
       attribute "lifetime", "number"
+      attribute "grid_power", "string"
+      attribute "load_power", "string"
+      attribute "pv_power", "string"
       attribute "energy_tile", "string"
       attribute "overview_tile", "string"
+      attribute "power_flow_tile", "string"
     }
 
     preferences {
@@ -45,9 +53,196 @@ metadata {
           input("site_id", "number", title: "Site Id", description: "Site Id to monitor.", required: true)
           input("api_key", "text", title: "API Key", description: "API key for given site id. This key is generated using installer level permissions. Ask installer to generate key or setup as sub-account with the correct permissions. Or sign up as an installer and take ownership of the inverter.", required: true)
           input("url", "text", title: "Solaredge Monitor API URL", description: "Endpoint URL for Solaredge Monitoring API.", required: true, defaultValue: "https://monitoringapi.solaredge.com")
-          input("refresh_interval", "enum", title: "How often to refresh data", options: ["10", "15", "30"], required: true)
+
+          input("energy_refresh_interval", "enum", title: "How often to refresh the energy data", options: [
+            255:"Do NOT update",
+            1:"1 Minute",
+            2:"2 Minutes",
+            3:"3 Minutes",
+            4:"4 Minutes",
+            5:"5 Minutes",
+            6:"6 Minutes",
+            7:"7 Minutes",
+            8:"8 Minutes",
+            9:"9 Minutes",
+            10:"10 Minutes",
+            11:"11 Minutes",
+            12:"12 Minutes",
+            13:"13 Minutes",
+            14:"14 Minutes",
+            15:"15 Minutes",
+            16:"16 Minutes",
+            17:"17 Minutes",
+            18:"18 Minutes",
+            19:"19 Minutes",
+            20:"20 Minutes",
+            21:"21 Minutes",
+            22:"22 Minutes",
+            23:"23 Minutes",
+            24:"24 Minutes",
+            25:"25 Minutes",
+            26:"26 Minutes",
+            27:"27 Minutes",
+            28:"28 Minutes",
+            29:"29 Minutes",
+            30:"30 Minutes",
+            31:"31 Minutes",
+            32:"32 Minutes",
+            33:"33 Minutes",
+            34:"34 Minutes",
+            35:"35 Minutes",
+            36:"36 Minutes",
+            37:"37 Minutes",
+            38:"38 Minutes",
+            39:"39 Minutes",
+            40:"40 Minutes",
+            41:"41 Minutes",
+            42:"42 Minutes",
+            43:"43 Minutes",
+            44:"44 Minutes",
+            45:"45 Minutes",
+            46:"46 Minutes",
+            47:"47 Minutes",
+            48:"48 Minutes",
+            49:"49 Minutes",
+            50:"50 Minutes",
+            51:"51 Minutes",
+            52:"52 Minutes",
+            53:"53 Minutes",
+            54:"54 Minutes",
+            55:"55 Minutes",
+            56:"56 Minutes",
+            57:"57 Minutes",
+            58:"58 Minutes",
+            59:"59 Minutes"
+          ], required: true)
+          input("overview_refresh_interval", "enum", title: "How often to refresh the production overview data", options: [
+            255:"Do NOT update",
+            1:"1 Minute",
+            2:"2 Minutes",
+            3:"3 Minutes",
+            4:"4 Minutes",
+            5:"5 Minutes",
+            6:"6 Minutes",
+            7:"7 Minutes",
+            8:"8 Minutes",
+            9:"9 Minutes",
+            10:"10 Minutes",
+            11:"11 Minutes",
+            12:"12 Minutes",
+            13:"13 Minutes",
+            14:"14 Minutes",
+            15:"15 Minutes",
+            16:"16 Minutes",
+            17:"17 Minutes",
+            18:"18 Minutes",
+            19:"19 Minutes",
+            20:"20 Minutes",
+            21:"21 Minutes",
+            22:"22 Minutes",
+            23:"23 Minutes",
+            24:"24 Minutes",
+            25:"25 Minutes",
+            26:"26 Minutes",
+            27:"27 Minutes",
+            28:"28 Minutes",
+            29:"29 Minutes",
+            30:"30 Minutes",
+            31:"31 Minutes",
+            32:"32 Minutes",
+            33:"33 Minutes",
+            34:"34 Minutes",
+            35:"35 Minutes",
+            36:"36 Minutes",
+            37:"37 Minutes",
+            38:"38 Minutes",
+            39:"39 Minutes",
+            40:"40 Minutes",
+            41:"41 Minutes",
+            42:"42 Minutes",
+            43:"43 Minutes",
+            44:"44 Minutes",
+            45:"45 Minutes",
+            46:"46 Minutes",
+            47:"47 Minutes",
+            48:"48 Minutes",
+            49:"49 Minutes",
+            50:"50 Minutes",
+            51:"51 Minutes",
+            52:"52 Minutes",
+            53:"53 Minutes",
+            54:"54 Minutes",
+            55:"55 Minutes",
+            56:"56 Minutes",
+            57:"57 Minutes",
+            58:"58 Minutes",
+            59:"59 Minutes"
+          ], required: true)
+          input("power_flow_refresh_interval", "enum", title: "How often to refresh power flow data", options: [
+            255:"Do NOT update",
+            1:"1 Minute",
+            2:"2 Minutes",
+            3:"3 Minutes",
+            4:"4 Minutes",
+            5:"5 Minutes",
+            6:"6 Minutes",
+            7:"7 Minutes",
+            8:"8 Minutes",
+            9:"9 Minutes",
+            10:"10 Minutes",
+            11:"11 Minutes",
+            12:"12 Minutes",
+            13:"13 Minutes",
+            14:"14 Minutes",
+            15:"15 Minutes",
+            16:"16 Minutes",
+            17:"17 Minutes",
+            18:"18 Minutes",
+            19:"19 Minutes",
+            20:"20 Minutes",
+            21:"21 Minutes",
+            22:"22 Minutes",
+            23:"23 Minutes",
+            24:"24 Minutes",
+            25:"25 Minutes",
+            26:"26 Minutes",
+            27:"27 Minutes",
+            28:"28 Minutes",
+            29:"29 Minutes",
+            30:"30 Minutes",
+            31:"31 Minutes",
+            32:"32 Minutes",
+            33:"33 Minutes",
+            34:"34 Minutes",
+            35:"35 Minutes",
+            36:"36 Minutes",
+            37:"37 Minutes",
+            38:"38 Minutes",
+            39:"39 Minutes",
+            40:"40 Minutes",
+            41:"41 Minutes",
+            42:"42 Minutes",
+            43:"43 Minutes",
+            44:"44 Minutes",
+            45:"45 Minutes",
+            46:"46 Minutes",
+            47:"47 Minutes",
+            48:"48 Minutes",
+            49:"49 Minutes",
+            50:"50 Minutes",
+            51:"51 Minutes",
+            52:"52 Minutes",
+            53:"53 Minutes",
+            54:"54 Minutes",
+            55:"55 Minutes",
+            56:"56 Minutes",
+            57:"57 Minutes",
+            58:"58 Minutes",
+            59:"59 Minutes"
+          ], required: true)
           input("energy_title", "text", title: "Daily Energy Details Title", description: "Title for energy details tile. Empty value removes title from tile.", required: false)
           input("overview_title", "text", title: "Production Overview Title", description: "Title for solar produtcion overview tile. Empty value removes title from tile.", required: false)
+          input("power_flow_title", "text", title: "Power Flow Title", description: "Title for current flow overview(PV, load, and grid). Empty value removes title from tile.", required: false)
           input(name: "display_last_updated", type: "bool", title: "Display last updated timestamp?", defaultValue: false)
           input(name: "debug", type: "bool", title: "Enable debug logging", defaultValue: false)
       }
@@ -57,17 +252,7 @@ metadata {
 def installed() {
 	log.info "Solaredge Monitoring has been installed."
 
-  delayBetween([
-    sendEvent(name: "feed_in", value: 0),
-    sendEvent(name: "self_consumption", value: 0),
-    sendEvent(name: "purchased", value: 0),
-    sendEvent(name: "consumption", value: 0),
-    sendEvent(name: "production", value: 0),
-    sendEvent(name: "power", value: 0),
-    sendEvent(name: "last_day", value: 0),
-    sendEvent(name: "last_month", value: 0),
-    sendEvent(name: "last_year", value: 0),
-    sendEvent(name: "lifetime", value: 0)])
+  clearData()
 
   state.version = version()
 }
@@ -76,6 +261,9 @@ def uninstalled() {
 	if (debug) log.info "Solaredge Monitoring has been uninstalled."
 
   unschedule(refresh)
+  unschedule(queryEnergyEndpoint)
+  unschedule(queryOverviewEndpoint)
+  unschedule(querySitePowerFlowEndpoint)
 }
 
 def refresh() {
@@ -84,6 +272,8 @@ def refresh() {
 
   queryEnergyEndpoint()
   queryOverviewEndpoint()
+  querySitePowerFlowEndpoint()
+
   updateTiles()
 
   state.last_updated = new Date().format("YYYY-MM-dd HH:mm:ss")
@@ -95,10 +285,58 @@ def updated() {
       log.debug "Settings updated."
   }
 
-  schedule("0 */${settings.refresh_interval} * ? * * *", refresh)
+  unschedule(refresh)
+  unschedule(queryEnergyEndpoint)
+  unschedule(queryOverviewEndpoint)
+  unschedule(querySitePowerFlowEndpoint)
+
+  if( settings.energy_refresh_interval != "255") schedule("0 */${settings.energy_refresh_interval} * ? * * *", queryEnergyEndpoint)
+  if( settings.overview_refresh_interval != "255") schedule("0 */${settings.overview_refresh_interval} * ? * * *", queryOverviewEndpoint)
+  if( settings.power_flow_refresh_interval != "255") schedule("0 */${settings.power_flow_refresh_interval} * ? * * *", querySitePowerFlowEndpoint)
+
   updateTiles()
 
   state.version = version()
+}
+
+def getEnergy() {
+  queryEnergyEndpoint()
+  updateTiles()
+}
+
+def getOverview() {
+  queryOverviewEndpoint()
+  updateTiles()
+}
+
+def getSitePowerFlow() {
+  querySitePowerFlowEndpoint()
+  updateTiles()
+}
+
+def clearData()
+{
+  if (debug) log.debug "Deleting all metrics."
+
+  delayBetween([
+    sendEvent(name: "feed_in", value: 0),
+    sendEvent(name: "self_consumption", value: 0),
+    sendEvent(name: "purchased", value: 0),
+    sendEvent(name: "consumption", value: 0),
+    sendEvent(name: "production", value: 0),
+    sendEvent(name: "power", value: 0),
+    sendEvent(name: "last_day", value: 0),
+    sendEvent(name: "last_month", value: 0),
+    sendEvent(name: "last_year", value: 0),
+    sendEvent(name: "grid_power", value: "0 kW"),
+    sendEvent(name: "load_power", value: "0 kW"),
+    sendEvent(name: "pv_power", value: "0 kW"),
+    sendEvent(name: "energy_tile", value: ""),
+    sendEvent(name: "overview_tile", value: ""),
+    sendEvent(name: "power_flow_tile", value: ""),
+    sendEvent(name: "lifetime", value: 0)])
+
+    updateTiles()
 }
 
 def queryEnergyEndpoint() {
@@ -232,6 +470,56 @@ def queryOverviewEndpoint() {
   }
 }
 
+def querySitePowerFlowEndpoint() {
+
+  if (debug) log.debug "Gathering site power flow data."
+
+  def params = [
+    uri: "${settings.url}",
+    path: "/site/${settings.site_id}/currentPowerFlow",
+    query: [api_key: "${settings.api_key}"]
+  ]
+
+  try {
+    httpGet(params) { r ->
+
+      if (debug) {
+        log.debug "Status: ${r.getStatus()}"
+        log.debug "Headers: ${r.getAllHeaders()}"
+        log.debug "Content Type: ${r.getContentType()}"
+        log.debug "Response: ${r.data}"
+      }
+
+      sendEvent(name: "grid_power", value: r.data.siteCurrentPowerFlow.GRID.currentPower + " " + r.data.siteCurrentPowerFlow.unit)
+      sendEvent(name: "load_power", value: r.data.siteCurrentPowerFlow.LOAD.currentPower + " " + r.data.siteCurrentPowerFlow.unit)
+      sendEvent(name: "pv_power", value: r.data.siteCurrentPowerFlow.PV.currentPower + " " + r.data.siteCurrentPowerFlow.unit)
+
+    }
+  } catch (Exception e) {
+      log.error "Exception"
+
+      if(e.getStatusCode()) {
+        switch(e.getStatusCode()) {
+            case 401:
+                log.error "401 - Not authorized"
+                break
+            case 403:
+                log.error "403 - Forbidden"
+                break
+            case 429:
+                log.error "429 - Too many requests"
+                break
+            default:
+                log.error "Unkown Error"
+                log.error "httpGet status code: e.getStatusCode ${e.getStatusCode()}"
+                log.error "httpGet message: e.message : ${e.message}"
+                log.error "httpGet full stack: e : ${e}"
+                break
+      }
+    }
+  }
+}
+
 def updateTiles() {
 
   if (debug) log.debug "Updating tile information."
@@ -289,6 +577,15 @@ def updateTiles() {
 
   sendEvent(name: "overview_tile", value: overview_tile)
 
+  def power_flow_tile = "<div style='font-size: 15px;'><table width='100%'>"
+  if (settings.power_flow_title) power_flow_tile += "<tr><td style='text-align: center; width: 100%'>" + settings.power_flow_title + "</td></tr>"
+  if (device.currentValue("pv_power")) power_flow_tile += "<tr><td style='text-align: left; width: 100%'>" + "Solar: <span style='color: green;'>" + device.currentValue("pv_power") + "</span></td></tr>"
+  if (device.currentValue("grid_power")) power_flow_tile += "<tr><td style='text-align: left; width: 100%'>" + "Grid: <span style='color: red;'>" + device.currentValue("grid_power") + "</span></td></tr>"
+  if (device.currentValue("load_power")) power_flow_tile += "<tr><td style='text-align: left; width: 100%'>" + "Usage: <span style='color: red;'>" + device.currentValue("load_power") + "</span></td></tr>"
+  if (settings.display_last_updated == true) power_flow_tile += "<tr><td style='text-align: center; width: 100%'><br />Last Updated: " + state.last_updated + "</td></tr>"
+  power_flow_tile += "</table></div>"
+
+  sendEvent(name: "power_flow_tile", value: power_flow_tile)
 }
 
 private formatEnergy(energy)
